dbb7fc91b2f273719de703fb32eee0322227c112
Charlotte Shih <shih.charlotte@gmail.com>
2019-05-16 22:04:13 -0700

    Fixes headers error

--- server.js
+++ server.js
@@ -26,22 +26,22 @@ app.listen(PORT, () => {
 function handleServerError(error, req, res, next) {
     let response
 
-    if(process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production') {
         response = { error:  { message: 'server error' } }
     }
     else {
         response = { error }
     }
 
-    res.status(500).json(response)
+    return res.status(500).json(response)
 }
 
 function validateApiKey(req, res, next) {
     const userKey = process.env.API_KEY
     const authKey = req.get('Authorization')
 
-    if(!authKey || authKey.split(' ')[1] !== userKey) {
-        res.status(401).json({ error: "Unauthorized request" })
+    if (!authKey || authKey.split(' ')[1] !== userKey) {
+        return res.status(401).json({ error: "Unauthorized request" })
     }
     else {
         next()
@@ -52,35 +52,35 @@ function respondToGetRequest(req, res) {
     const { genre, country, avg_vote } = req.query
     let response = moviedb
 
-    if(genre) {
+    if (genre) {
         response = response.filter(movie => {
             return movie.genre.toLowerCase().includes(genre.toLowerCase())
         })
-        if(!response.length) {
-            res.status(400).json({ "Error": "No movies with that genre!" })
+        if (!response.length) {
+            return res.status(400).json({ "Error": "No movies with that genre!" })
         }
     }
 
-    if(country) {
+    if (country) {
         response = response.filter(movie => {
             return movie.country.toLowerCase().includes(country.toLowerCase())
         })
-        if(!response.length) {
-            res.status(400).json({ "Error": "No movies in that country!" })
+        if (!response.length) {
+            return res.status(400).json({ "Error": "No movies in that country!" })
         }
     }
 
-    if(avg_vote) {
-        if(isNaN(avg_vote)) {
-            res.status(400).json({ "Error": "Average vote must be a number!" })
+    if (avg_vote) {
+        if (isNaN(avg_vote)) {
+            return res.status(400).json({ "Error": "Average vote must be a number!" })
         }
-        if(avg_vote >= 10 || avg_vote < 1) {
-            res.status(400).json({ "Error": "Average vote must be between 0 and 9.9!" })
+        if (avg_vote >= 10 || avg_vote < 1) {
+            return res.status(400).json({ "Error": "Average vote must be between 1 and 9.9!" })
         }
         response = response.filter(movie => {
             return movie.avg_vote >= avg_vote
         })
     }
 
-    res.status(200).json(response)
+    return res.status(200).json(response)
 }
